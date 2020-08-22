3fd943e4691723773f9aabbd1d5a6c7dbf6d6cdb
Krxwallo <a@b.c>
2020-08-22 18:10:33 +0200

    Cleaned up some code...
    Signed-off-by: Krxwallo <a@b.c>

--- src/main/java/com/mrcrayfish/glasscutter/client/ClientHandler.java
+++ src/main/java/com/mrcrayfish/glasscutter/client/ClientHandler.java
@@ -15,37 +15,6 @@ public class ClientHandler
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GLASSCUTTER, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GLASS_STAIRS, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GLASS_SLAB, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WHITE_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ORANGE_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MAGENTA_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIGHT_BLUE_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.YELLOW_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIME_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GRAY_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIGHT_GRAY_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CYAN_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PURPLE_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BLUE_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BROWN_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GREEN_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RED_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BLACK_STAINED_GLASS_STAIRS, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WHITE_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ORANGE_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MAGENTA_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIGHT_BLUE_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.YELLOW_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIME_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GRAY_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.LIGHT_GRAY_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CYAN_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PURPLE_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BLUE_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BROWN_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GREEN_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RED_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BLACK_STAINED_GLASS_SLAB, RenderLayer.getTranslucent());
+        ModBlocks.getTranslucentBlocks().forEach((block -> BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getTranslucent())));
     }
 }
--- src/main/java/com/mrcrayfish/glasscutter/init/ModBlocks.java
+++ src/main/java/com/mrcrayfish/glasscutter/init/ModBlocks.java
@@ -12,60 +12,32 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 
 /**
  * Author: MrCrayfish
- *
- * <strong>Do not change the field names!</strong>
  */
 public class ModBlocks
 {
     public static Block GLASSCUTTER;
     public static Block GLASS_STAIRS;
     public static Block GLASS_SLAB;
-    public static Block WHITE_STAINED_GLASS_STAIRS;
-    public static Block ORANGE_STAINED_GLASS_STAIRS;
-    public static Block MAGENTA_STAINED_GLASS_STAIRS;
-    public static Block LIGHT_BLUE_STAINED_GLASS_STAIRS;
-    public static Block YELLOW_STAINED_GLASS_STAIRS;
-    public static Block LIME_STAINED_GLASS_STAIRS;
-    public static Block PINK_STAINED_GLASS_STAIRS;
-    public static Block GRAY_STAINED_GLASS_STAIRS;
-    public static Block LIGHT_GRAY_STAINED_GLASS_STAIRS;
-    public static Block CYAN_STAINED_GLASS_STAIRS;
-    public static Block PURPLE_STAINED_GLASS_STAIRS;
-    public static Block BLUE_STAINED_GLASS_STAIRS;
-    public static Block BROWN_STAINED_GLASS_STAIRS;
-    public static Block GREEN_STAINED_GLASS_STAIRS;
-    public static Block RED_STAINED_GLASS_STAIRS;
-    public static Block BLACK_STAINED_GLASS_STAIRS;
-    public static Block WHITE_STAINED_GLASS_SLAB;
-    public static Block ORANGE_STAINED_GLASS_SLAB;
-    public static Block MAGENTA_STAINED_GLASS_SLAB;
-    public static Block LIGHT_BLUE_STAINED_GLASS_SLAB;
-    public static Block YELLOW_STAINED_GLASS_SLAB;
-    public static Block LIME_STAINED_GLASS_SLAB;
-    public static Block PINK_STAINED_GLASS_SLAB;
-    public static Block GRAY_STAINED_GLASS_SLAB;
-    public static Block LIGHT_GRAY_STAINED_GLASS_SLAB;
-    public static Block CYAN_STAINED_GLASS_SLAB;
-    public static Block PURPLE_STAINED_GLASS_SLAB;
-    public static Block BLUE_STAINED_GLASS_SLAB;
-    public static Block BROWN_STAINED_GLASS_SLAB;
-    public static Block GREEN_STAINED_GLASS_SLAB;
-    public static Block RED_STAINED_GLASS_SLAB;
-    public static Block BLACK_STAINED_GLASS_SLAB;
+
+    public static ArrayList<Block> getTranslucentBlocks() {
+        return translucentBlocks;
+    }
+
+    private static final ArrayList<Block> translucentBlocks = new ArrayList<>();
     private static final Logger LOGGER = LogManager.getLogger();
 
     /**
      * Try to register the blocks via reflection. (not that repetitive). Trows tons of reflection exceptions.
-     * @throws NoSuchFieldException when there are no fields found. probably someone has renamed the fields...
+     * @throws NoSuchFieldException when a field, e.g. {@link Blocks#WHITE_STAINED_GLASS} wasn't found.
      * @throws NoSuchMethodException when a method wasn't found.
      * @throws InstantiationException when an error occurs during the {@link StainedGlassSlabBlock} or {@link StainedGlassStairsBlock} constructor call.
      */
-    public static void registerAll() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException {
+    public static void registerAll() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException, NoSuchFieldException {
         // Glasscutter
         GLASSCUTTER = register("glasscutter", new GlasscutterBlock(AbstractBlock.Settings.of(Material.STONE).strength(3.5F)), ItemGroup.DECORATIONS);
         // Glass Stairs
@@ -76,16 +48,15 @@ public class ModBlocks
         for (DyeColor color: DyeColor.values()) {
             String[] types = new String[] { "stairs", "slab" };
             for (String type : types) {
-                // Get the field.
-                Field blockField = ModBlocks.class.getField(color.getName().toUpperCase() + "_STAINED_GLASS_" + type.toUpperCase());
                 // Get the StainedGlassStairsBlock or the StainedGlassSlabBlock constructor.
                 Constructor<?> constructor = type.equals("stairs") ? StainedGlassStairsBlock.class.getConstructor(DyeColor.class, BlockState.class,
                         AbstractBlock.Settings.class) : StainedGlassSlabBlock.class.getConstructor(DyeColor.class, BlockState.class, AbstractBlock.Settings.class);
                 // Get the corresponding block in the Blocks class.
                 Block block = (Block) Blocks.class.getDeclaredField(color.getName().toUpperCase() + "_STAINED_GLASS").get(Blocks.class);
                 // Set the field to the (registered) block.
-                blockField.set(ModBlocks.class, register(color.getName() + "_stained_glass_" + type,
-                        (Block) constructor.newInstance(color, block.getDefaultState(), AbstractBlock.Settings.copy(block))));
+                block = register(color.getName() + "_stained_glass_" + type,
+                        (Block) constructor.newInstance(color, block.getDefaultState(), AbstractBlock.Settings.copy(block)));
+                translucentBlocks.add(block);
             }
         }
     }
@@ -103,7 +74,7 @@ public class ModBlocks
 
     /**
      * Register a block and the corresponding block item.
-     * @param name the name of the block, e.g. for {@link #WHITE_STAINED_GLASS_SLAB} "white_stained_glass_slab"
+     * @param name the name of the block, e.g. "white_stained_glass_slab"
      * @param block the actual {@link ItemGroup}
      * @param group the {@link ItemGroup}
      * @return the (registered) block
